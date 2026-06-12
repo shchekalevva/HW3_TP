1475046c42a731bee63b4f0489a7c4b25ae5251a
shchekalevva <alexa131829@gmail.com>
2026-06-12 18:47:29 +0000

    Настроен генератор расписания тренировок

--- generate.py
+++ generate.py
@@ -6,15 +6,15 @@ import sys
 NUM_ROWS = 50
 
 
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["Workout_Name", "Day_Of_The_Week", "Room_Number", "Trainer"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "Workout_Name": random.choice(["Йога", "Кроссфит", "Пилатес", "Зумба", "TRX", "Стретчинг"]),
+        "Day_Of_The_Week": random.choice(["Понедельник", "Среда", "Пятница", "Суббота"]),
+        "Room_Number": random.randint(1, 6),
+        "Trainer": random.choice(["Александра", "Евгений", "Мария", "Дарья", "Андрей"])
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
