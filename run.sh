fe595f4df4299fb400224e086cad9035da4378d5
shchekalevva <alexa131829@gmail.com>
2026-06-12 19:32:36 +0000

    Добавлен Dockerfile, команды build и run в bash-скрипт

--- run.sh
+++ run.sh
@@ -3,6 +3,13 @@ COMMAND=$1
 if [ "$COMMAND" == "create_local_data" ]; then
     echo "Создание локальных данных"
     python generate.py local_data
+elif [ "$COMMAND" == "build_generator" ]; then
+    echo "Сборка образа генератора"
+    docker build -f Dockerfile.generator -t my_generator .
+
+elif [ "$COMMAND" == "run_generator" ]; then
+    echo "Запуск генератора"
+    docker run --rm -v ./data:/data my_generator
 else
     echo "Неизвестная команда '$COMMAND'./run.sh create_local_data\nДоступные команды: create_local_data"
     exit 1
